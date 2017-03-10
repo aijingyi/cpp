36b1a7715bb3c9d705ee3aa45dc97c7950568f35
aijingyi <kevinlaung@sina.com>
2017-03-10 08:42:55 +0800

    add t8_1.cpp

---
+++ cppTest/8/t8_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Time
+{
+public:
+    void set_time(void);
+    void show_time(void);
+private:
+    int hour;
+    int minute;
+    int sec;
+};
+
+int main()
+{
+Time t;
+t.set_time();
+t.show_time();
+return 0;
+}
+
+void Time::set_time(void)
+{
+    cin>>hour;
+    cin>>minute;
+    cin>>sec;
+
+}
+void Time::show_time(void)
+{
+    cout<<hour<<":"<<minute<<":"<<sec<<endl;
+}
